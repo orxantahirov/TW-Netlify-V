8b660333e65c01064311b0608a88f4ff35f3a31a
Musa Musazada <musaam@code.edu.az>
2022-01-24 13:46:27 +0400

    nav bar ug fixed

--- src/index.js
+++ src/index.js
@@ -29,24 +29,11 @@ window.onload = () => {
     const subLinks = document.querySelectorAll(".sub-primary");
     const menus = document.querySelectorAll(".menu");
 
-    menus.forEach((el) => {
-      el.addEventListener("mouseover", function () {
-        document.body.style.height = "100vh";
-        document.body.style.overflowY = "hidden";
-      });
-      el.addEventListener("mouseleave", function () {
-        document.body.style.height = "auto";
-        document.body.style.overflowY = "visible";
-      });
-    });
-
     subLinks.forEach((el) =>
       el.addEventListener("mouseover", function () {
         menus.forEach((el) => {
           el.style.height = "";
           el.style.overflow = "hidden";
-          document.body.style.height = "auto";
-          document.body.style.overflowY = "visible";
         });
       })
     );
@@ -62,11 +49,7 @@ window.onload = () => {
         el.nextElementSibling.style.overflow = "visible";
         if (el.nextElementSibling.scrollHeight < 447) {
           el.nextElementSibling.style.height = "447px";
-          document.body.style.height = "auto";
-          document.body.style.overflowY = "visible";
         } else {
-          document.body.style.height = "100%";
-          document.body.style.overflowY = "hidden";
           el.nextElementSibling.style.height =
             el.nextElementSibling.scrollHeight + "px";
         }
@@ -79,8 +62,6 @@ window.onload = () => {
         el.addEventListener("mouseleave", () => {
           el.style.height = "";
           el.style.overflow = "hidden";
-          document.body.style.height = "auto";
-          document.body.style.overflowY = "visible";
         })
       );
     });
